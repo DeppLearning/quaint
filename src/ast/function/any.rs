83b4cb936eef2472d48c592ecf6aac4ff3baa84e
Andrej Krakau <krakau@filancore.com>
2023-09-12 22:11:43 +0200

    Revert "remove incorrect doc commets"
    This reverts commit 35c85f11366e107c6f9d64300c87bd6f7c7413cf.

--- src/ast/function/any.rs
+++ src/ast/function/any.rs
@@ -2,10 +2,23 @@ use super::Function;
 use crate::ast::Expression;
 
 #[derive(Debug, Clone, PartialEq)]
+/// A date function that coerces expression results into dates.
 pub struct Any<'a> {
     pub(crate) value: Box<Expression<'a>>,
 }
 
+/// Coerces the given expression result into Date.
+///
+/// ```rust
+/// # use quaint::{ast::*, visitor::{Visitor, Sqlite}};
+/// # fn main() -> Result<(), quaint::error::Error> {
+/// let query = Select::from_table("users").value(date(Column::new("createdAt")));
+///
+/// let (sql, _) = Sqlite::build(query)?;
+/// assert_eq!("SELECT DATE(`createdAt`) FROM `users`", sql);
+/// # Ok(())
+/// # }
+/// ```
 pub fn any<'a, T>(expr: T) -> Function<'a>
 where
     T: Into<Expression<'a>>,
